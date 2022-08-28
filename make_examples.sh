1fb84eea102cdc26223c89f01c3d99b2c19fbda4
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-08-28 13:23:35 +0200

    adding more exampels

--- make_examples.sh
+++ make_examples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for n3 in list/*.n3 math/*.n3 string/*.n3 ; do
+for n3 in list/*.n3 log/*.n3 math/*.n3 string/*.n3 ; do
     eye --nope --quiet --pass-only-new $n3 > $n3.out 2> /dev/null
 done
