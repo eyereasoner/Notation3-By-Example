c5a1b63d1e26bd42f2a89edacc981291ecb598d8
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2022-09-01 10:08:21 +0200

    Adding an annotated blogic example

--- make_examples.sh
+++ make_examples.sh
@@ -3,3 +3,7 @@
 for n3 in list/*.n3 log/*.n3 math/*.n3 string/*.n3 ; do
     eye --nope --quiet --pass-only-new $n3 > $n3.out 2> /dev/null
 done
+
+for n2 in log/blogic/*.n3 ; do 
+    eye --nope --blogic $n3 > $n3.out 2> /dev/null
+done
