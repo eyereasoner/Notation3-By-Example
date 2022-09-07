3985d6049fa40cd1c29e39bcf5900a33ed75228f
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2022-09-07 06:49:12 +0200

    Fixing make script

--- make_examples.sh
+++ make_examples.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for n3 in list/*.n3 log/*.n3 math/*.n3 string/*.n3 ; do
+for n3 in crypto/*.n3 graph/*.n3 list/*.n3 log/*.n3 math/*.n3 string/*.n3 ; do
     eye --nope --quiet --pass-only-new $n3 > $n3.out 2> /dev/null
 done
 
-for n2 in log/blogic/*.n3 ; do 
-    eye --nope --blogic $n3 > $n3.out 2> /dev/null
+for n3 in log/blogic/*.n3 ; do 
+    eye --nope --quiet --pass-only-new --blogic $n3 > $n3.out 2> /dev/null
 done
