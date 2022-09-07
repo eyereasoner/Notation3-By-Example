29eade353374cf3474c9e29c4bf8f5109f8e27c1
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2022-09-07 07:35:37 +0200

    Adding a tester for the .out files

--- make_examples.sh
+++ make_examples.sh
@@ -7,3 +7,15 @@ done
 for n3 in log/blogic/*.n3 ; do 
     eye --nope --quiet --pass-only-new --blogic $n3 > $n3.out 2> /dev/null
 done
+
+for f in $(find . -name "*.out") ; do
+    echo -n "Testing $f ... "
+
+    if [[ $f =~ blogic ]]; then 
+        echo "SKIPPED"
+    elif [[ $(grep '<urn:example:is> true' $f) ]]; then
+        echo "OK"
+    else
+        echo "FAILED"
+    fi
+done
