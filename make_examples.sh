c2522594bfb7d91e1f3e5f82d528f9f351d824b8
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2022-09-08 14:56:18 +0200

    Adding special outputString processing

--- make_examples.sh
+++ make_examples.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 for n3 in crypto/*.n3 graph/*.n3 list/*.n3 log/*.n3 math/*.n3 string/*.n3 ; do
-    eye --nope --quiet --pass-only-new --strings $n3 > $n3.out 2> /dev/null
+    if [[ "$n3" == "log/outputString.n3" ]] ; then
+        eye --nope --quiet --pass --strings $n3 > $n3.out 2> /dev/null
+    else
+        eye --nope --quiet --pass $n3 > $n3.out 2> /dev/null
+    fi
 done
 
 for n3 in log/blogic/*.n3 ; do 
-    eye --nope --quiet --pass-only-new --blogic $n3 > $n3.out 2> /dev/null
+    eye --nope --quiet --pass --blogic $n3 > $n3.out 2> /dev/null
 done
 
 for f in $(find . -name "*.out") ; do
