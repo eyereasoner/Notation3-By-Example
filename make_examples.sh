2e56fba8a33a978ec0ce5497948229cf223c15be
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-09-12 07:47:49 +0200

    Adding a simple forward and backward chaining example

--- make_examples.sh
+++ make_examples.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for n3 in crypto/*.n3 graph/*.n3 list/*.n3 log/*.n3 math/*.n3 string/*.n3 ; do
+if [ "$1" == "clean" ]; then
+    rm *.out 2> /dev/null
+    for n3 in crypto graph list log math string time ; do
+        rm $n3/*.out 2> /dev/null
+    done
+    exit 0
+fi 
+
+for n3 in *.n3 crypto/*.n3 graph/*.n3 list/*.n3 log/*.n3 math/*.n3 string/*.n3 time/*.n3 ; do
     if [[ "$n3" == "log/outputString.n3" ]] ; then
         eye --nope --quiet --pass --strings $n3 > $n3.out 2> /dev/null
     else
