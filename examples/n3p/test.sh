c81b1b4560d50c2bbec9532d8657bcd8c93d6f64
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-11-04 06:13:10 +0100

    Adding output of n3p scripts to git

--- examples/n3p/test.sh
+++ examples/n3p/test.sh
@@ -5,4 +5,6 @@ eye --nope --quiet --pass --intermediate demo.n3p demo.n3 > /dev/null 2>&1
 eye --nope --quiet --n3p demo.n3p 2> /dev/null
 
 # Running own n3p code
-eye --nope --quiet --n3p demo2.n3p 2> /dev/null
+eye --nope --quiet --n3p demo2.n3p > demo2.out.n3 2> /dev/null
+
+eye --nope --quiet --n3p demo3.n3p > demo3.out.n3 2> /dev/null
