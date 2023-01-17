643ee93b63968419bee64e4c260bdc8ed280cc5d
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-01-17 10:43:57 +0100

    Adding an example how contradictions can be detected with rules

---
+++ examples/contradict/test-blogic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This should fail
+eye --blogic --quiet blogic.n3 owl-disjointWith-blogic.n3 owl-differentFrom-blogic.n3 
---
+++ examples/contradict/test-n3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This should fail
+eye --nope --quiet --pass notation3.n3 owl-disjointWith.n3 owl-differentFrom.n3 
