d0faddd783a65bfacc0ace54349e8c257dc183de
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-04-18 06:50:18 +0200

    Adding support skipping over SKIP files

--- make_examples.sh
+++ make_examples.sh
@@ -16,6 +16,8 @@ for n3 in *.n3 crypto/*.n3 graph/*.n3 list/*.n3 log/*.n3 math/*.n3 string/*.n3 ;
     if [[ "$n3" == "log/outputString.n3" ]] ; then
         echo "eye --nope --quiet --pass --strings $n3 > $n3.out 2> /dev/null"
         eye --nope --quiet --pass --strings $n3 > $n3.out 2> /dev/null
+    elif [[ "$n3" =~ SKIP ]] ; then
+        echo "(skipping $n3)" | tee $n3.out
     else
         echo "eye --nope --quiet --pass $n3 > $n3.out 2> /dev/null"
         eye --nope --quiet --pass $n3 > $n3.out 2> /dev/null
@@ -23,8 +25,12 @@ for n3 in *.n3 crypto/*.n3 graph/*.n3 list/*.n3 log/*.n3 math/*.n3 string/*.n3 ;
 done
 
 for n3 in log/blogic/*.n3 ; do 
-    echo "eye --nope --quiet --blogic $n3 > $n3.out 2> /dev/null"
-    eye --nope --quiet --blogic $n3 > $n3.out 2> /dev/null
+    if [[ "$n3" =~ SKIP ]] ; then
+        echo "(skipping $n3)" | tee $n3.out
+    else
+        echo "eye --nope --quiet --blogic $n3 > $n3.out 2> /dev/null"
+        eye --nope --quiet --blogic $n3 > $n3.out 2> /dev/null
+    fi
 done
 
 for f in $(find . -name "*.out" | sort) ; do
