ac42168eed89ad8eba835bc119b54affbf52a4c1
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-06-13 07:18:19 +0200

    Adding a general valid checker (keep other checks simple)

--- examples/sharcs/test.sh
+++ examples/sharcs/test.sh
@@ -3,4 +3,4 @@
 DATA=$(find . -name "*.ttl")
 
 # login
-eye --nope --quiet --blogic ${DATA} check_login.n3s check_permissions.n3s query.n3s
+eye --nope --quiet --blogic ${DATA} check_*.n3s query.n3s
