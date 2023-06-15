8d14b31cfe792a224e7fab922e6dfa83da8e9021
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-06-15 10:17:59 +0200

    Adding transaction usage contract policy evaluation

--- examples/sharcs/data_provider_session.sh
+++ examples/sharcs/data_provider_session.sh
@@ -5,4 +5,10 @@ DATA=$(find . -name "*.ttl")
 # Run 1: reasoning part
 eye --nope --quiet --blogic ${DATA} \
     policies/data_provider/check_*.n3s \
-    policies/data_provider/query.n3s 
+    policies/data_provider/query.n3s \
+    |\
+    # Run 2: validation part (allows SNAF queries)
+    eye --nope --quiet --blogic - \
+        validation/data_provider/valid_*.n3s \
+        validation/data_provider/policies/check_*.n3s \
+        validation/data_provider/query.n3s 
