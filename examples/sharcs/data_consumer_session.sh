d1ff6b8dd24319038328f6a7cc9cdcdb1b7821cd
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-06-16 08:34:17 +0200

    Refactor

--- examples/sharcs/data_consumer_session.sh
+++ examples/sharcs/data_consumer_session.sh
@@ -5,10 +5,11 @@ DATA=$(find grant organizations pmp policies validation -name "*.ttl")
 # Run 1: reasoning part
 eye --nope --quiet --blogic ${DATA} data_consumer_session.ttl \
     policies/data_consumer/check_*.n3s \
-    policies/data_consumer/query.n3s \
+    policies/query.n3s \
     |\
     # Run 2: validation part (allows SNAF queries)
     eye --nope --quiet --blogic - \
+        validation/validator.n3s \
         validation/data_consumer/valid_*.n3s \
         validation/data_consumer/policies/check_*.n3s \
-        validation/data_consumer/query.n3s
+        validation/query.n3s
--- examples/sharcs/data_provider_session.sh
+++ examples/sharcs/data_provider_session.sh
@@ -5,10 +5,11 @@ DATA=$(find grant organizations pmp policies validation -name "*.ttl")
 # Run 1: reasoning part
 eye --nope --quiet --blogic ${DATA} data_provider_session.ttl \
     policies/data_provider/check_*.n3s \
-    policies/data_provider/query.n3s \
+    policies/query.n3s \
     |\
     # Run 2: validation part (allows SNAF queries)
     eye --nope --quiet --blogic - \
+        validation/validator.n3s \
         validation/data_provider/valid_*.n3s \
         validation/data_provider/policies/check_*.n3s \
-        validation/data_provider/query.n3s 
+        validation/query.n3s 
