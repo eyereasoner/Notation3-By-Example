e3373344ee8a8d29ccbe231861dfda561800996d
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-06-16 09:08:13 +0200

    Adding output

--- examples/sharcs/data_consumer_session.sh
+++ examples/sharcs/data_consumer_session.sh
@@ -7,9 +7,13 @@ eye --nope --quiet --blogic ${DATA} data_consumer_session.ttl \
     policies/data_consumer/check_*.n3s \
     policies/query.n3s \
     |\
+    tee -a data_consumer_session.step1.ttl \
+    |\
     # Run 2: validation part (allows SNAF queries)
     eye --nope --quiet --blogic - \
         validation/validator.n3s \
         validation/data_consumer/valid_*.n3s \
         validation/data_consumer/policies/check_*.n3s \
-        validation/query.n3s
+        validation/query.n3s \
+        |\
+        tee -a data_consumer_session.step2.ttl 
--- examples/sharcs/data_provider_session.sh
+++ examples/sharcs/data_provider_session.sh
@@ -7,9 +7,13 @@ eye --nope --quiet --blogic ${DATA} data_provider_session.ttl \
     policies/data_provider/check_*.n3s \
     policies/query.n3s \
     |\
+    tee -a data_provider_session.step1.ttl \
+    |\
     # Run 2: validation part (allows SNAF queries)
     eye --nope --quiet --blogic - \
         validation/validator.n3s \
         validation/data_provider/valid_*.n3s \
         validation/data_provider/policies/check_*.n3s \
-        validation/query.n3s 
+        validation/query.n3s \
+        |\
+        tee -a data_provider_session.step2.ttl 
