960cd7bc8066ef1602c0d98d017d988933137aad
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-06-24 06:31:09 +0200

    Fixing matching the policy itself

--- examples/sharcs-odrl/test.sh
+++ examples/sharcs-odrl/test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-eye --nope --quiet --pass-only-new \
-            compiler.n3 util.n3 data_consumer_policy.ttl > data_consumer_policy.n3s
-
-eye --nope --quiet --blogic \
-            data.ttl data_consumer_policy.ttl \
-            data_consumer_policy.n3s data_consumer_policy.q
+eye --nope --quiet --pass-only-new util.n3 test.n3
