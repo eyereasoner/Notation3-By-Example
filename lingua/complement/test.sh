ccf978170fdd8e6b77c718da92b4a0ce2dca10f4
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2024-02-18 06:19:30 +0100

    Some refactoring

--- lingua/complement/test.sh
+++ lingua/complement/test.sh
@@ -2,5 +2,5 @@
 
 for f in test*.trig; do
     echo "test $f..."
-    eye --quiet complement.trig disjointWith.trig symmetricProperty.trig  $f 2>&1 | tee $f.answer
+    eye --quiet complement.trig symmetricProperty.trig irreflexiveProperty.trig  $f 2>&1 | tee $f.answer
 done
